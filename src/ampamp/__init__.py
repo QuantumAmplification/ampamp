98e0e17cc1ea178f2de2aecde382424fe7fd154f
varun29-git <vjdaiya29@gmail.com>
2026-03-16 19:46:07 +0530

    Update __init__.py

--- src/ampamp/__init__.py
+++ src/ampamp/__init__.py
@@ -1,19 +1,27 @@
+# src/ampamp/__init__.py
 from .foundations import GroverEngine
 from .fixed_point import FixedPointEngine
 from .oblivious import ObliviousEngine
 from .foqa import FOQAEngine
-from .distributed import DQAAEngine, OracleSynthesizer # <--- New
+from .distributed import DQAAEngine, OracleSynthesizer
+from .variable_time import VTAAEngine, VariableTimeBranch
+from .qsvt import SU2QSPEngine, QSVTSynthesizer  
 
 from .diagnostics import (
     GroverAuditor, 
     FPAAAuditor, 
     ObliviousAuditor,
     FOQAAuditor,
-    DistributedAuditor # <--- New
+    DistributedAuditor,
+    VTAAAuditor,
+    FundamentalLimitsAuditor,
+    QSVTAuditor  
 )
 
 __all__ = [
     "GroverEngine", "FixedPointEngine", "ObliviousEngine", "FOQAEngine", 
-    "DQAAEngine", "OracleSynthesizer",
-    "GroverAuditor", "FPAAAuditor", "ObliviousAuditor", "FOQAAuditor", "DistributedAuditor"
+    "DQAAEngine", "OracleSynthesizer", "VTAAEngine", "VariableTimeBranch",
+    "SU2QSPEngine", "QSVTSynthesizer",
+    "GroverAuditor", "FPAAAuditor", "ObliviousAuditor", "FOQAAuditor", 
+    "DistributedAuditor", "VTAAAuditor", "FundamentalLimitsAuditor", "QSVTAuditor"
 ]
