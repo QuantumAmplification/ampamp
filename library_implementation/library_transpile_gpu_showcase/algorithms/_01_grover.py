c3068d27c6c262753372cc225da7a4d133866add
varun29-git <vjdaiya29@gmail.com>
2026-05-11 20:16:15 +0530

    remove folders

--- library_implementation/library_transpile_gpu_showcase/algorithms/_01_grover.py
+++
@@ -1,6 +0,0 @@
-from qiskit import QuantumCircuit
-from ampamp.grover import GroverEngine
-
-
-def build_circuit() -> QuantumCircuit:
-    return GroverEngine(6, [10, 25]).construct_circuit(1)
--- library_implementation/library_transpile_gpu_showcase/algorithms/_07_unified_comparative.py
+++
@@ -1,6 +0,0 @@
-from qiskit import QuantumCircuit
-from ._02_fixed_point import build_circuit as build_fixed_point
-
-
-def build_circuit() -> QuantumCircuit:
-    return build_fixed_point()
